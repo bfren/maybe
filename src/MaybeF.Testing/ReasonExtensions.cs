2de588da94e13e4a992bf4be0b03fc1981f16c34
Ben Green <ben@bcgdesign.com>
2022-03-27 12:30:16 +0100

    Adding AssertType method - #42

---
+++ src/MaybeF.Testing/ReasonExtensions.cs
@@ -0,0 +1,9 @@
+// Maybe: .NET Monad.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
+
+namespace MaybeF.Testing;
+
+/// <summary>
+/// <see cref="IReason"/> Extensions
+/// </summary>
+public static partial class ReasonExtensions { }
