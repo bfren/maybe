b20a7513a6becf817d8e3385886630ee2753febb
bfren <ben@bcgdesign.com>
2022-04-18 16:45:48 +0100

    Adding RemoveValue tests - #57

--- tests/Tests.MaybeF.Caching/_/MaybeCache/RemoveValue_Tests.cs
+++ tests/Tests.MaybeF.Caching/_/MaybeCache/RemoveValue_Tests.cs
@@ -1,8 +1,24 @@
 // Maybe: Unit Tests
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
 
+using Microsoft.Extensions.Caching.Memory;
+
 namespace MaybeF.Caching.MaybeCache_Tests;
 
 public class RemoveValue_Tests
 {
+	[Fact]
+	public void Calls_Cache_Remove__With_Correct_Values()
+	{
+		// Arrange
+		var mc = Substitute.For<IMemoryCache>();
+		var cache = new MaybeCache<string>(mc);
+		var key = Rnd.Str;
+
+		// Act
+		cache.RemoveValue(key);
+
+		// Assert
+		mc.Received().Remove(key);
+	}
 }
