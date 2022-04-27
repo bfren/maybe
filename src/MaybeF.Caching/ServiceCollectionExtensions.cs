80af9b269c07aaa3f12cb750e5e71bfc503997c2
bfren <ben@bcgdesign.com>
2022-04-27 10:24:11 +0100

    Always add cache as Singleton - #72

--- src/MaybeF.Caching/ServiceCollectionExtensions.cs
+++ src/MaybeF.Caching/ServiceCollectionExtensions.cs
@@ -11,30 +11,11 @@ namespace MaybeF.Caching;
 public static class ServiceCollectionExtensions
 {
 	/// <summary>
-	/// Add a <see cref="IMaybeCache{TKey}"/> to <paramref name="this"/> with a Scoped lifetime
+	/// Add a <see cref="IMaybeCache{TKey}"/> to <paramref name="this"/> with a Singleton lifetime
 	/// </summary>
 	/// <typeparam name="TKey">Cache Key type</typeparam>
 	/// <param name="this"></param>
 	public static IServiceCollection AddMaybeCache<TKey>(this IServiceCollection @this)
 		where TKey : notnull =>
-		AddMaybeCache<TKey>(@this, false);
-
-	/// <summary>
-	/// Add a <see cref="IMaybeCache{TKey}"/> to <paramref name="this"/> -<br/>
-	///  - if <paramref name="singleton"/> is true it will have a Singleton lifetime
-	///  - if <paramref name="singleton"/> is false it will have a Scoped lifetime
-	/// </summary>
-	/// <typeparam name="TKey">Cache Key type</typeparam>
-	/// <param name="this"></param>
-	/// <param name="singleton"></param>
-	public static IServiceCollection AddMaybeCache<TKey>(this IServiceCollection @this, bool singleton)
-		where TKey : notnull =>
-		singleton switch
-		{
-			true =>
-				@this.AddSingleton<IMaybeCache<TKey>, MaybeCache<TKey>>(),
-
-			false =>
-				@this.AddScoped<IMaybeCache<TKey>, MaybeCache<TKey>>(),
-		};
+		@this.AddSingleton<IMaybeCache<TKey>, MaybeCache<TKey>>();
 }
